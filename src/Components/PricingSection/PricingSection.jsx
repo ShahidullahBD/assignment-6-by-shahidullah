f877f8693f5575021122d6945815f2205d59a3ef
ShahidullahBD <shahidullahkorea@gmail.com>
2026-04-01 21:49:52 +0900

    last update

--- src/Components/PricingSection/PricingSection.jsx
+++ src/Components/PricingSection/PricingSection.jsx
@@ -13,7 +13,7 @@ const PricingSection = () => {
                 <p className='mb-5'>Choose the plan that fits your needs. Upgrade or downgrade anytime.</p>
             </div>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 my-10'>
-                <div className='flex flex-col p-6 border rounded-2xl space-y-3 relative'>                    
+                <div className='flex flex-col p-6 border rounded-2xl space-y-3 relative shadow-lg'>                    
                     <h3 className='text-2xl font-bold'>Starter</h3>
                     <p>Perfect for getting started</p>
                     <p><span className='text-2xl font-bold'>$0</span>/Month</p>
@@ -37,7 +37,7 @@ const PricingSection = () => {
                     </div>
                     <button className={`${bgLinear} btn btn-primary w-full rounded-full text-white mt-auto`}>Get Started Free</button>
                 </div>
-                <div className={`${bgLinear} p-6 border rounded-2xl space-y-3 relative text-white flex flex-col`}>                    
+                <div className={`${bgLinear} p-6 border rounded-2xl space-y-3 relative text-white flex flex-col shadow-lg`}>                    
                     <p className='absolute -top-3.5 right-27 px-3 py-1 bg-amber-400 rounded-full'>Most Popular</p>
                     <h3 className='text-2xl font-bold'>Pro</h3>
                     <p>Best for professionals</p>
@@ -71,7 +71,7 @@ const PricingSection = () => {
                     <button className={`btn w-full rounded-full text-blue-800 mt-auto`}> 
                         <span className={`${textLinear}`}>Start Pro Trial</span> </button>
                 </div>
-                <div className='flex flex-col p-6 border rounded-2xl space-y-3 relative'>                    
+                <div className='flex flex-col p-6 border rounded-2xl space-y-3 relative shadow-lg'>                    
                     <h3 className='text-2xl font-bold'>Enterprise</h3>
                     <p>For teams and businesses</p>
                     <p><span className='text-2xl font-bold'>$0</span>/Month</p>
